28e46e6c3f5d83398d92f57e1b8953e13255e662
marcocentofanti <marcocentofanti@hotmail.it>
2025-12-10 17:46:52 +0100

    Restart a mente +-fresca

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,15 +10,10 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = [null];
-
-for (let i = 0; i < teachers.length; i++ ) {
-  if( i === 3) {
-    fourthTeacher[0] = (teachers[i])
-    // 
-  }
-}
+const fourthTeacher = teachers[3];
 console.log(fourthTeacher)
+
+
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
